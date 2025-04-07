f886464644043bca56dac509e5adc97f789d44ed
johndoesstuff <johndoesstuff03@gmail.com>
2025-04-07 12:14:12 -0400

    print board

--- board.py
+++ board.py
@@ -6,6 +6,37 @@ class Board:
         self.cols = 6
         self.connections = connections
         self.requireds = requireds
+        self.board = [[Cell.EMPTY for _ in range(6)] for _ in range(6)]
+        for requirement in requireds:
+            self.board[requirement.pos.row][requirement.pos.col] = requirement.cell
+
+    def print_board(self):
+        cell_symbols = {
+            Cell.EMPTY: " . ",
+            Cell.SUN: " O ",
+            Cell.MOON: " C "
+        }
+
+        def get_relation(pos1, pos2):
+            for conn in self.connections:
+                if (conn.posA == pos1 and conn.posB == pos2) or (conn.posB == pos1 and conn.posA == pos2):
+                    return "=" if conn.relation == Relation.EQ else "x"
+            return " "  # No connection
+
+        for row in range(self.rows):
+            line = ""
+            for col in range(self.cols):
+                line += cell_symbols[self.board[row][col]]
+                if col < self.cols - 1:
+                    line += get_relation(Position(row, col), Position(row, col + 1))
+            print(line)
+            if row < self.rows - 1:
+                between = " "
+                for col in range(self.cols):
+                    between += get_relation(Position(row, col), Position(row + 1, col))
+                    if col < self.cols - 1:
+                        between += "   "  # space between vertical connections
+                print(between)
 
 
 class Connection:
@@ -27,6 +58,12 @@ class Position:
             self.col = col
         else:
             raise Exception("Col must be between 0 and 5")
+    def __eq__(self, other):
+        return isinstance(other, Position) and self.row == other.row and self.col == other.col
+
+    def __hash__(self):
+        return hash((self.row, self.col))
+
 
 
 class Required:
@@ -54,3 +91,20 @@ def checkAdj(posA, posB):
         return True
     else:
         return False
+
+
+requireds = [
+    Required(Position(0, 0), Cell.SUN),
+    Required(Position(0, 1), Cell.MOON),
+    Required(Position(1, 0), Cell.MOON),
+    Required(Position(1, 1), Cell.SUN),
+]
+
+connections = [
+    Connection(Position(0, 0), Position(0, 1), Relation.NEQ),
+    Connection(Position(0, 0), Position(1, 0), Relation.EQ),
+    Connection(Position(0, 1), Position(1, 1), Relation.EQ),
+]
+
+board = Board(connections, requireds)
+board.print_board()
